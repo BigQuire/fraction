3fb790812e300257d8a29cd237c5335df2f7742a
Cornelius <liewsansze@gmail.com>
2026-05-22 18:49:26 +0800

    add options

--- fraction-backend/server.js
+++ fraction-backend/server.js
@@ -14,6 +14,7 @@ app.use(cors({origin: 'https://fraction-chi.vercel.app',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }))
+app.options('*', cors())
 app.use(express.json())
 app.use('/api/users', userRoutes)
 app.use('/api/artworks', artworkRoutes)
